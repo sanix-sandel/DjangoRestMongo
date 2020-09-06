8766fc1db384ab72af72e2a1dfe79e0996acb203
sanix sandel <sanicksikani@gmail.com>
2020-09-06 12:53:04 +0300

    views and urls

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns=[
+    path('/api/posts', views.PostListView.as_view()),
+    path('/api/<int:pk>', views.PostDetailView.as_view()),
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, generics
 
-# Create your views here.
+
+
+class PostListView(generics.ListCreateApiView):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer=PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response({}, status=400)
+
+class PostDetailView(APIView):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer 
+
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNotExist:
+            raise Http404
+            
+    def get(self, request, pk, format=None):
+        post=self.get_object(pk)
+        serializer=PostSerializer(post)
+        return Response(serializer.data)        
+    
+    def put(self, request, pk, format=None):
+        post=self.get_object(pk)
+        serializer=PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400) 
+
+    def delete(self, request, pk, format=None):
+        post=self.get_object(pk)
+        post.delete()
+        return Response(status=204)            
